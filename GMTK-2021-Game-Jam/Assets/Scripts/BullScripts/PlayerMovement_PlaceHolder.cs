ecd028677f97d8eece780d4d2afdd382a494a3d7
Matthew Lucas <mlucas.github@gmail.com>
2021-06-12 13:00:20 +1000

    Rigidbody character movement
    Changed movement over to rigidbody.

--- GMTK-2021-Game-Jam/Assets/Scripts/BullScripts/PlayerMovement_PlaceHolder.cs
+++ GMTK-2021-Game-Jam/Assets/Scripts/BullScripts/PlayerMovement_PlaceHolder.cs
@@ -9,21 +9,40 @@ public class PlayerMovement_PlaceHolder : MonoBehaviour
     public float speed;
 
     public InputAction wasd;
+    public InputAction ijkl;
     public CharacterController controller;
+    public Rigidbody rb;
+
+    public bool is2nd;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void OnEnable()
     {
         wasd.Enable();
+        ijkl.Enable();
     }
 
     private void OnDisable()
     {
         wasd.Disable();
+        ijkl.Disable();
     }
     private void Update()
     {
-        Vector2 inputVector = wasd.ReadValue<Vector2>();
-        Vector3 finalVector = new Vector3(inputVector.x,0,inputVector.y);
-        controller.Move(finalVector * Time.deltaTime * speed);
+        Vector2 inputVector = new Vector2();
+
+        if (is2nd)
+            inputVector = wasd.ReadValue<Vector2>() * speed;
+        else if(!is2nd)
+            inputVector = ijkl.ReadValue<Vector2>() * speed;
+
+        Vector3 finalVector = new Vector3(inputVector.x,rb.velocity.y,inputVector.y);
+        rb.velocity = finalVector;
+        //controller.Move(finalVector * Time.deltaTime * speed);
     }
 
  
