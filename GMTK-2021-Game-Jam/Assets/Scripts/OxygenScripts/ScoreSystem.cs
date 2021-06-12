0d7f007cc68a9889250368c82b7fb05a4ce1a42a
oxygencraft <21054297+oxygencraft@users.noreply.github.com>
2021-06-12 14:13:59 +1000

    Add score system

---
+++ GMTK-2021-Game-Jam/Assets/Scripts/OxygenScripts/ScoreSystem.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public int Score
+    {
+        get
+        {
+            int obstacles = ObstacleCollisions * -200;
+            if (obstacles == 0)
+                obstacles = 1000;
+
+            int time = 0;
+            if (TimeElapsed > 300)
+            {
+                time -= 100 * (TimeElapsed - 300);
+            }
+            else
+            {
+                time += 100 * (300 - TimeElapsed);
+            }
+
+            return time + obstacles + Bonuses;
+        }
+    }
+
+    public int HighScore => PlayerPrefs.GetInt("highScore", 0);
+
+    public int ObstacleCollisions { get; private set; }
+    public int TimeElapsed { get; private set; }
+    public int Bonuses { get; private set; }
+    public int BonusesCount { get; private set; }
+
+    public void IncrementObstacleCollision()
+    {
+        ObstacleCollisions++;
+    }
+
+    public void AddBonus(int bonus)
+    {
+        Bonuses += bonus;
+        BonusesCount++;
+    }
+
+    public void SaveHighScore()
+    {
+        if (Score > HighScore)
+        {
+            PlayerPrefs.SetInt("highScore", HighScore);
+            PlayerPrefs.Save();
+        }
+    }
+
+    private IEnumerator UpdateTime()
+    {
+        for (;;)
+        {
+            TimeElapsed++;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+    
+    public void StartScoreTime()
+    {
+        StartCoroutine(nameof(UpdateTime));
+    }
+
+    public void StopScoreTime()
+    {
+        StopAllCoroutines();
+    }
+
+    public static ScoreSystem instance;
+    
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+            Destroy(this.gameObject);
+    }
+}
