075cc6b0cabbba470728a803efcae58cff4a26f6
oxygencraft <21054297+oxygencraft@users.noreply.github.com>
2021-06-13 13:44:28 +1000

    Swap player 1 and 2 fish powerups

--- GMTK-2021-Game-Jam/Assets/Scripts/OxygenScripts/FishPowerup.cs
+++ GMTK-2021-Game-Jam/Assets/Scripts/OxygenScripts/FishPowerup.cs
@@ -9,26 +9,26 @@ public class FishPowerup : Powerup
     
     public override void GivePlayer1Powerup(GameObject player1, GameObject player2)
     {
+        // TODO: Add invincibility
+    }
+
+    public override void GivePlayer2Powerup(GameObject player1, GameObject player2)
+    {
         player1.GetComponent<PlayerMovement>().speed += speedIncrease;
         player2.GetComponent<PlayerMovement>().speed += speedIncrease;
         StartCoroutine(ResetPlayer1Powerup(player1, player2, speedIncreaseTime));
     }
 
-    public override void GivePlayer2Powerup(GameObject player1, GameObject player2)
-    {
-        // TODO: Add invincibility
-    }
-
     public override IEnumerator ResetPlayer1Powerup(GameObject player1, GameObject player2, float delay)
     {
         yield return new WaitForSeconds(delay);
-        player1.GetComponent<PlayerMovement>().speed -= speedIncrease;
-        player2.GetComponent<PlayerMovement>().speed -= speedIncrease;
+        // TODO: Reset invincibility
     }
 
     public override IEnumerator ResetPlayer2Powerup(GameObject player1, GameObject player2, float delay)
     {
         yield return new WaitForSeconds(delay);
-        // TODO: Reset invincibility
+        player1.GetComponent<PlayerMovement>().speed -= speedIncrease;
+        player2.GetComponent<PlayerMovement>().speed -= speedIncrease;
     }
 }
