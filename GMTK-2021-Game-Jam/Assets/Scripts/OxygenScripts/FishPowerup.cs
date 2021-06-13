be4bcffb1a9d2d2653eb32dbdd6a450a75f48454
oxygencraft <21054297+oxygencraft@users.noreply.github.com>
2021-06-13 13:12:25 +1000

    Add powerups

---
+++ GMTK-2021-Game-Jam/Assets/Scripts/OxygenScripts/FishPowerup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FishPowerup : Powerup
+{
+    public float speedIncrease;
+    public float speedIncreaseTime;
+    public float invincibilityTime;
+    
+    public override void GivePlayer1Powerup(GameObject player1, GameObject player2)
+    {
+        player1.GetComponent<PlayerMovement>().speed += speedIncrease;
+        player2.GetComponent<PlayerMovement>().speed += speedIncrease;
+        StartCoroutine(ResetPlayer1Powerup(player1, player2, speedIncreaseTime));
+    }
+
+    public override void GivePlayer2Powerup(GameObject player1, GameObject player2)
+    {
+        // TODO: Add invincibility
+    }
+
+    public override IEnumerator ResetPlayer1Powerup(GameObject player1, GameObject player2, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        player1.GetComponent<PlayerMovement>().speed -= speedIncrease;
+        player2.GetComponent<PlayerMovement>().speed -= speedIncrease;
+    }
+
+    public override IEnumerator ResetPlayer2Powerup(GameObject player1, GameObject player2, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        // TODO: Reset invincibility
+    }
+}
---
+++ GMTK-2021-Game-Jam/Assets/Scripts/OxygenScripts/Powerup.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public abstract class Powerup : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != 6)
+            return;
+        
+        GameObject player1 = GameObject.FindWithTag("Player1");
+        GameObject player2 = GameObject.FindWithTag("Player2");
+        GameObject player = other.gameObject;
+
+        if (player.CompareTag("Player1"))
+        {
+            GivePlayer1Powerup(player1, player2);
+        }
+        else if (player.CompareTag("Player2"))
+        {
+            GivePlayer2Powerup(player1, player2);
+        }
+
+        Destroy(gameObject.GetComponent<MeshRenderer>());
+        Destroy(gameObject.GetComponent<Collider>());
+    }
+
+    public abstract void GivePlayer1Powerup(GameObject player1, GameObject player2);
+    public abstract void GivePlayer2Powerup(GameObject player1, GameObject player2);
+    public abstract IEnumerator ResetPlayer1Powerup(GameObject player1, GameObject player2, float delay);
+    public abstract IEnumerator ResetPlayer2Powerup(GameObject player1, GameObject player2, float delay);
+}
